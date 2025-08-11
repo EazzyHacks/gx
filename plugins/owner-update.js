6989605c320d5408ac8104ba509a3b6807cc2344
EazzyHacks <yhair7200@gmail.com>
2025-08-11 09:36:13 +0200

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -32,6 +32,6 @@ let handler = async (m, { conn }) => {
 handler.help = ['update']
 handler.tags = ['owner']
 handler.command = ['update']
-handler.rowner = true
+handler.rowner = false
 
 export default handler
